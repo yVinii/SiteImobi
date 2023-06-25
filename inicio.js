f4deab7527035da79c33f96e40303093915757a3
yVinii <silveirafabrini@gmail.com>
2023-06-25 20:48:30 -0300

    Slider não responsivo

--- inicio.js
+++ inicio.js
@@ -24,3 +24,20 @@ window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 50);
 });
+
+//javscript slider
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval( function(){
+    nextImage();
+}, 15000);
+
+function nextImage(){
+    count++;
+    if(count>4){
+        count = 1;
+   }
+
+   document.getElementById("radio"+count).checked = true;
+}
