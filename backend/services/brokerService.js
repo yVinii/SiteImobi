60d8b992abcc1185daa9dc53d84e209d03860669
David Lucas <davidlucaskl@gmail.com>
2024-06-05 14:58:34 -0300

    Corretor sendo preenchido na tela funcionando

--- backend/services/brokerService.js
+++ backend/services/brokerService.js
@@ -1,82 +1,94 @@
-const BrokerRepository = require('../repositories/brokerRepository');
-
+const BrokerRepository = require("../repositories/brokerRepository");
+const PropertiesRepository = require("../repositories/PropertiesRepository");
 module.exports = class BrokerService {
-    static async createBroker(data){
-        const { name, email, phone, creci } = data;
-        if (!name || !email || !phone || !creci) {
-            throw new Error('Todos os campos são obrigatórios');
-        }
-        const brokerExist = await BrokerRepository.findByEmail(email);
-        if (brokerExist) {
-            throw new Error('Por favor, utilize outro email');
-        }
-        await BrokerRepository.createBroker({ name, email, phone, creci, active: true });
+  static async createBroker(data) {
+    const { name, email, phone, creci } = data;
+    if (!name || !email || !phone || !creci) {
+      throw new Error("Todos os campos são obrigatórios");
+    }
+    const brokerExist = await BrokerRepository.findByEmail(email);
+    if (brokerExist) {
+      throw new Error("Por favor, utilize outro email");
+    }
+    await BrokerRepository.createBroker({
+      name,
+      email,
+      phone,
+      creci,
+      active: true,
+    });
+  }
+
+  static async getById(id) {
+    const brokerExist = await BrokerRepository.getById(id);
+    if (!brokerExist) {
+      throw new Error("Corretor não cadastrado ou inativo");
+    }
+    return brokerExist;
+  }
+
+  static async getAll() {
+    return await BrokerRepository.getAll();
+  }
+
+  static async update(id, data) {
+    const { name, email, phone, creci } = data;
+    if (!name || !email || !phone || !creci) {
+      throw new Error("Todos os campos são obrigatórios");
+    }
+    const brokerExist = await BrokerRepository.findByEmail(email);
+    if (brokerExist) {
+      throw new Error("Por favor, utilize outro email");
     }
 
-    static async getById(id) {
-        const brokerExist = await BrokerRepository.getById(id);
-        if(!brokerExist){
-            throw new Error('Corretor não cadastrado ou inativo');
-        }
-        return brokerExist;
+    let updateData = {};
+    //validations
+
+    if (!name) {
+      throw new Error("o nome é obrigatório");
+    } else {
+      updateData.name = name;
     }
 
-    static async getAll() {
-        return await BrokerRepository.getAll();
+    if (!email) {
+      throw new Error("o email é obrigatório");
+    } else {
+      updateData.email = email;
     }
 
-    static async update(id, data) {
-        const { name, email, phone, creci } = data;
-        if (!name || !email || !phone || !creci) {
-            throw new Error('Todos os campos são obrigatórios');
-        }
-        const brokerExist = await BrokerRepository.findByEmail(email);
-        if (brokerExist) {
-            throw new Error('Por favor, utilize outro email');
-        }
-
-        let updateData = {};
-        //validations
-
-       if(!name){
-        throw new Error('o nome é obrigatório')
-       } else{
-        updateData.name = name
-       }
-
-       if(!email){
-        throw new Error('o email é obrigatório')
-       } else{
-        updateData.email = email
-       }
-
-       if(!phone){
-        throw new Error('o phone é obrigatório')
-       } else{
-        updateData.phone = phone
-       }
-
-       if(!creci){
-        throw new Error('o creci é obrigatório')
-       } else{
-        updateData.creci = creci
-       }
-       // Update data object
-        updateData = {
-          name,
-          email,
-          phone,
-          creci,
-      };
-        return await BrokerRepository.update(id, updateData);
+    if (!phone) {
+      throw new Error("o phone é obrigatório");
+    } else {
+      updateData.phone = phone;
     }
 
-    static async delete(id) {
-
-        const brokerExist = await BrokerRepository.getById(id);
-        if (!brokerExist) {
-            throw new Error('Corretor não pode ser excluido');
-        }
-        return await BrokerRepository.delete(id);
+    if (!creci) {
+      throw new Error("o creci é obrigatório");
+    } else {
+      updateData.creci = creci;
     }
-}
+    // Update data object
+    updateData = {
+      name,
+      email,
+      phone,
+      creci,
+    };
+    return await BrokerRepository.update(id, updateData);
+  }
+
+  static async delete(id) {
+    const brokerExist = await BrokerRepository.getById(id);
+    const properties = await PropertiesRepository.getAllBrokerProperties(id);
+
+    if (!brokerExist) {
+      throw new Error("Corretor não pode ser excluido");
+    }
+    if (properties) {
+      throw new Error(
+        "Corretor não pode ser excluido, Propriedades vinculadas"
+      );
+    }
+    return await BrokerRepository.delete(id);
+  }
+};
---
+++ javascript/Fetchs/PainelControle/CorretoresPainel/carregarCorretores.js
@@ -0,0 +1,121 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const modal = document.getElementById("deleteModal");
+  const closeModal = document.getElementById("closeModal");
+  const confirmDelete = document.getElementById("confirmDelete");
+  const cancelDelete = document.getElementById("cancelDelete");
+
+  debugger;
+
+  closeModal.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+
+  cancelDelete.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+
+  confirmDelete.addEventListener("click", async () => {
+    if (currentBrokerId) {
+      const url = `http://localhost:5502/broker/${currentBrokerId}`;
+
+      try {
+        const token = getAuthToken();
+
+        const response = await fetch(url, {
+          method: "DELETE",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        if (response.ok) {
+          alert("Corretor excluído com sucesso!");
+          location.reload();
+        } else {
+          const errorText = await response.text();
+          console.error(
+            "Erro ao excluir corretor:",
+            response.status,
+            errorText
+          );
+          alert("Erro ao excluir corretor.", errorText.message);
+        }
+      } catch (error) {
+        console.error("Erro na requisição de exclusão:", error.message);
+        alert("Erro ao excluir corretor.", error.message);
+      }
+    }
+    modal.style.display = "none";
+  });
+
+  window.addEventListener("click", (event) => {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+  carregarCorretores();
+});
+
+async function carregarCorretores() {
+  try {
+    const response = await fetch("http://localhost:5502/broker/");
+    const data = await response.json();
+    debugger;
+    if (response.ok) {
+      const corretores = data.brokers;
+      corretores.forEach((broker) => CriarCardsCorretores(broker));
+      console.log("Corretores carregados com sucesso!");
+    } else {
+      console.error("Erro ao obter corretores:", response.statusText);
+    }
+  } catch (error) {
+    console.error("Erro na requisição:", error);
+  }
+}
+
+function CriarCardsCorretores(broker) {
+  const container = document.getElementById("broker-cards-container");
+
+  //const card = document.createElement("div");
+  const card2 = document.createElement("div");
+  //card.className = "card-funcionario";
+  card2.className = "card-funcionarios";
+
+  card2.innerHTML = `
+    <div class="image-content-funcionarios">
+      <span class="overlay-funcionarios"></span>
+      <div class="card-image">
+        <img src="../../" alt="" class="card-funcionarios-img">
+      </div>
+    </div>
+    <div class="card-funcionarios-content">
+      <h2 class="name-funcionario">${broker.name || "Nome corretor"}</h2>
+      <h2 class="funcao">${broker.email || "Email"}</h2>
+      <p class="name-funcionario">${broker.creci || "Email não disponível"}</p>
+       <p class="name-funcionario">${
+         broker.phone || "Telefone não disponível"
+       }</p>
+       </div>
+      <div class="card-funcionarios-button">
+        <button class="button btn-edit" value="${broker.id}">Editar</button>
+        <button class="button btn-delete" value="${broker.id}">Excluir</button>
+      </div>
+  `;
+
+  container.appendChild(card2);
+
+  const deleteButton = card2.querySelector(".btn-delete");
+
+  deleteButton.addEventListener("click", function () {
+    const modal = document.getElementById("deleteModal");
+    currentBrokerId = this.value;
+    modal.style.display = "flex";
+  });
+}
+
+function getAuthToken() {
+  return localStorage.getItem("token");
+}
